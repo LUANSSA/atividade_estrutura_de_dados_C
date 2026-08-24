a3bb511ed771eef22d946dddb5758fe2a7ee22f7
Vanessa <vanessak_gl@hotmail.com>
2026-08-23 21:57:10 -0300

    Comentários em structure.h / exclusão de arquivos 'teste' e '.gitignore'

--- grupo1/structure.h
+++ grupo1/structure.h
@@ -3,25 +3,38 @@
 
 #include "requisition.h"
 
+/* nó da estrutura - cada nó armazena uma requisição e aponta p/
+   o próximo nó
+*/ 
+
 typedef struct Node {
-    Requisition *requisition;
-    struct Node *next;
+    Requisition *requisition; // requisição armazenada
+    struct Node *next; // ponteiro para o próximo nó
 } Node;
+
+/* estrutura que representa a fila de requisições
+   'front' aponta p/ o primeiro e 'rear' p/ o último da fila
+*/
 
 typedef struct {
     Node *front;
     Node *rear;
-    int size;
+    int size; // quantidade de elementos na fila
 } Structure;
 
+// cria e inicia uma nova estrutura
 Structure* create_structure(void);
 
+// adiciona uma requisição ao final dessa estrutura
 int add(Structure *structure, Requisition *requisition);
 
+// remove e retorna a primeira requisição dessa estrutura
 Requisition* removal(Structure *structure);
 
+// retorna a quantidade de elementos dessa estrutura
 int get_size(Structure *structure);
 
+// libera a memória utilizada pela estrutura
 void free_structure(Structure *structure);
 
 #endif
